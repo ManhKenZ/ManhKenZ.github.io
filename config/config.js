55391d87d0f089b586b723963ba81bfd8e6d6f27
ManhKenZ <acc1discordloner@gmail.com>
2025-09-25 19:57:25 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,12 +4,12 @@ var config_aboutme = ['thathinh', 'chamngon'];
 var config_stk = ['mbbank', 'momo','vietinbank','vietcombank'];
 //=========================================================//
 var set_1 = {
-    fullname: 'Lương Đức Kiệt',
-    username: 'DucKietDzz',
-    website: 'hethongcheap.io.vn',
-    zalo: '09750170xx',
+    fullname: 'Nguyễn Phụ Mạnh',
+    username: 'ManhKenZ',
+    website: 'ManhKenZ.github.io',
+    zalo: '0395904226',
     avatar: 'https://img.upanh.tv/2023/12/17/F1A4CED5-18E2-46AA-8EBE-97B5DD195562.png',
-    facebook: 'https://fb.com/ad.duckietdz',
+    facebook: 'https://fb.com/KenzDev',
 };
 //=========================================================//
 var set_2 = {
@@ -19,20 +19,20 @@ var set_2 = {
 //=========================================================//
 var set_3 = {
     mbbank: {
-       sotaikhoan: '026620899999',
-        chutaikhoan: 'LUONG DUC KIET',
+       sotaikhoan: '0393889226',
+        chutaikhoan: 'NGUYEN PHU MANH',
     },
     momo: {
-        sotaikhoan: '0975017051',
-        chutaikhoan: 'LUONG DUC KIET',
+        sotaikhoan: '0395904226',
+        chutaikhoan: 'NGUYEN PHU MANH',
     },
     vietinbank: {
         sotaikhoan: 'Đang cập nhật...',
         chutaikhoan: 'Đang cập nhật...',
     },
     vietcombank: {
-        sotaikhoan: 'Đang cập nhật...',
-        chutaikhoan: 'Đang cập nhật...',
+        sotaikhoan: '0395904226',
+        chutaikhoan: 'NGUYEN PHU MANH',
     }
 };
 //=========================================================//
